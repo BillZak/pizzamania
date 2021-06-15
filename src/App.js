cbaefe400d8f9a2a4847cfe98ea153f8958aaf69
Bilal Zak <bzak954@gmail.com>
2021-06-15 14:15:37 +0000

    add components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
+import Navbar from './Components/Navbar';
+import Carousel from './Components/Carousel';
+import AboutPizza from './Components/AboutPizza';
+import PizzaTypes from './Components/PizzaTypes';
+import Footer from './Components/Footer';
+
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <Carousel />
+      <AboutPizza />
+      <PizzaTypes />
+      <Footer />
+    </>
   );
 }
 
